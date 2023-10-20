0ca0fa868bdee51cb9a7d04ff1bfed04d1ea9a26
Evan Lauer <evthestrike@gmail.com>
2023-10-20 15:36:49 -0600

    refactored bearerToken

--- app/Main.hs
+++ app/Main.hs
@@ -2,27 +2,34 @@
 
 module Main (main) where
 
-import Control.Arrow
 import Control.Lens
-import Course
-import Data.Aeson
-import Data.Aeson.Lens
 import qualified Data.ByteString as B
 import Data.Maybe
-import Data.Text
 import qualified Data.Vector as V
-import Data.Vector.Lens
+import Data.Word as W
 import Network.Wreq
 import ParseCourse
-
-bearerToken :: B.ByteString
-bearerToken = ""
+import System.IO
 
 queryDatabaseURL :: String
 queryDatabaseURL = "https://api.notion.com/v1/databases/96d3fb824abe41b1a4d9b9cc74341375/query"
 
+getBearerToken :: IO B.ByteString
+getBearerToken = do
+  tokenHandle <- openFile "bearerToken.txt" ReadMode
+  contents <- B.hGetContents tokenHandle
+  hClose tokenHandle
+
+  let newline :: Word8
+      newline = 10
+      bearerToken = head . B.split newline $ contents
+
+  return bearerToken
+
 main :: IO ()
 main = do
+  bearerToken <- getBearerToken
+
   let opts =
         defaults
           & auth ?~ oauth2Bearer bearerToken
